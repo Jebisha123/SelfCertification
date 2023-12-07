1e089986acdaaed3b915e4c0b0385c38ce971132
Jebisha <jebishanoj@gmail.com>
2023-12-07 11:09:11 +0530

    validations added

--- src/main/java/com/backend/service/model/User.java
+++ src/main/java/com/backend/service/model/User.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.access.method.P;
 
 @Entity // maps this User object with User table
 @AllArgsConstructor
@@ -19,11 +20,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Size(max = 20,min = 1)
-    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Input.FirstName Should contain only alphabets")
+    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid FirstName.FirstName Should contain only alphabets")
     @Column(nullable = false,length = 40)
     private String firstName;
     @Size(max = 20,min = 1)
-    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Input.LastName should contain only alphabets")
+    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid LastName.LastName should contain only alphabets")
     @Column(nullable = false,length = 40)
     private String lastName;
     @Column(nullable = false,length = 100)
@@ -31,10 +32,11 @@ public class User {
     private String email;
     @Size(max = 16,min = 8)
     @Column(nullable = false,length = 100)
-    @Pattern(regexp = "^[a-zA-Z0-9]+$",message = "Invalid username.UserName should alphanumeric")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$",message = "Invalid username.")
     private String username;
 //    @Size(min = 16)
     @Column(nullable = false,length = 100)
+    @Pattern(regexp = "^[^\\s]+$" ,message = "Invalid Password")
     private String password;
 //    @Column(nullable = false)
     private String role;
